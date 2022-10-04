f8e5fe23d610c7c69ed5459c4e3d80c658b03a5e
Kireshvanth <kiresh20122002@gmail.com>
2022-10-04 14:04:35 +0530

    Test shown 1

--- lib/login_screens/intro_screen.dart
+++ lib/login_screens/intro_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sem5_blood_mgmt/login_screens/sign_in.dart';
 
 class IntroScreen extends StatelessWidget {
 
@@ -17,12 +18,94 @@ class IntroScreen extends StatelessWidget {
 
             Container(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   SizedBox(
                     height: 80,
                   ),
                   Image.asset("assets/login_screens/intro_illustration.png"),
-                  Text("Donate"),
+                  Text(
+                    "LIFELINE",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35
+                    ),
+                  ),
+                  Text(
+                    "Donate Blood",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => SignInScreen()),
+                            );
+                          },
+                          child: Container(
+                            width: 100,
+                            height: 50,
+                            child: Center(
+                              child: Text(
+                                'Sign In',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            decoration: BoxDecoration(
+                              color: Color(0xffF4BCC1),
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(0xffF4BCC1),
+                                  offset:
+                                  Offset(2, 2), // changes position of shadow
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 100,
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.white,
+                                offset:
+                                Offset(2, 2), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -32,4 +115,5 @@ class IntroScreen extends StatelessWidget {
     );
   }
 }
+
 
